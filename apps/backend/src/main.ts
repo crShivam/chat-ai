62980702fe4121a246fe84dfa1d81f2e17277853
crShivam <crshivamjoshi.gmail.com>
2025-05-06 00:31:21 +0530

    feat: added backedn

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'verbose', 'debug'],
+    cors: {
+      origin: [
+        'http://localhost:5173',
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+      allowedHeaders: 'Content-Type,Authorization',
+    },
+    bodyParser: true,
+  });
+
+  // Swagger Setup
+  const config = new DocumentBuilder()
+    .setTitle('Notes AI API')
+    .setDescription('Notes AI API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory());
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+}
+bootstrap();
